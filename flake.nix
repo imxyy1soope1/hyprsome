98aa3f5c48dd61abc20d5134ac99c5f00530661a
imxyy_soope_ <3516554684@qq.com>
2024-07-07 20:58:49 +0800

    feat: overlay

--- flake.nix
+++ flake.nix
@@ -3,17 +3,31 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
     crane.inputs.nixpkgs.follows = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, crane, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        craneLib = crane.mkLib nixpkgs.legacyPackages.${system};
-      in
-    {
-      packages.default = craneLib.buildPackage {
+  outputs = { nixpkgs, crane, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      hyprsome = craneLib: craneLib.buildPackage {
         src = craneLib.cleanCargoSource (craneLib.path ./.);
       };
-    });
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          craneLib = crane.mkLib nixpkgs.legacyPackages.${system};
+        in
+          {
+            default = hyprsome craneLib;
+            hyprsome = hyprsome craneLib;
+          }
+      );
+      overlays.default = (final: prev: 
+      let
+        craneLib = crane.mkLib final;
+      in
+      {
+        hyprsome = hyprsome craneLib;
+      });
+    };
 }
